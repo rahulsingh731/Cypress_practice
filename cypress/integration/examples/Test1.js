2a6b5174fa915d55de1b3c6e1626020b5faa7628
Rahul Singh <rahulrocks731@gmail.com>
2024-12-29 01:18:31 +0530

    Added Alias & Cart feature

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -4,8 +4,16 @@ describe('Homepage', () => {
   it('should load successfully', () => {
     // Visit the homepage
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
-    cy.get('.search-keyword').type('ca');
-    cy.get('.products').find('.product').each(($el) => {
+    
+    // Alias for search input
+    cy.get('.search-keyword').as('searchInput');
+    
+    // Alias for products container
+    cy.get('.products').as('productsContainer');
+    
+    // Search for 'ca' and add Capsicum to cart
+    cy.get('@searchInput').type('ca');
+    cy.get('@productsContainer').find('.product').each(($el) => {
       const textVeg = $el.find('.product-name').text();
       if (textVeg.includes('Capsicum')) {
         cy.wrap($el).find('button').click();
@@ -16,9 +24,35 @@ describe('Homepage', () => {
     cy.get('.brand').then((logoElement) => {
       cy.log(logoElement.text());
     });
-
-    cy.get('.search-keyword').clear().type('cucu');
+    cy.get('.brand').should('have.text', 'GREENKART');
+    
+    // Search for 'cucu' and verify product name
+    cy.get('@searchInput').clear().type('cucu');
     cy.get('.product > .product-name').should('have.text', 'Cucumber - 1 Kg');
-    cy.get('.cart-icon > img').click();
+    
+    // Alias for cart icon
+    cy.get('.cart-icon > img').as('cartIcon');
+    
+    // Add Carrot to cart
+    cy.get('@cartIcon').click();
+    cy.get('@searchInput').clear().type('carrot');
+    cy.get('@productsContainer').find('.product').each(($el) => {
+      const textVeg = $el.find('.product-name').text();
+      if (textVeg.includes('Carrot')) {
+        cy.wrap($el).find('button').click();
+      }
+    });
+    cy.get('@cartIcon').click();
+    
+    // Proceed to checkout
+    cy.contains('PROCEED TO CHECKOUT').click();
+    
+    // Apply promo code
+    cy.get('.promoCode').type('rahulshettyacademy');
+    cy.get('.promoBtn').click();
+    cy.get('.promoInfo',{timeout:5000}).should('have.text', 'Code applied ..!');
+    
+    // Place order
+    cy.contains('Place Order').click();
   });
 });
