1bb89db0cd9cd3866791bc63c334ccb2dcc3a2cd
Rahul Singh <rahulrocks731@gmail.com>
2024-12-29 01:05:19 +0530

    optimized the code

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -4,15 +4,21 @@ describe('Homepage', () => {
   it('should load successfully', () => {
     // Visit the homepage
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
-    cy.wait(2000);
     cy.get('.search-keyword').type('ca');
-    // cy.get('.products > div').should('have.length', 4);
-    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
-    cy.wait(2000);
-    cy.get('.search-keyword').clear();
-    cy.get('.search-keyword').type('cucu');
-    cy.wait(2000);
-    cy.get('.product > .product-name').should('have.text', 'Cucumber - 1 Kg');
+    cy.get('.products').find('.product').each(($el) => {
+      const textVeg = $el.find('.product-name').text();
+      if (textVeg.includes('Capsicum')) {
+        cy.wrap($el).find('button').click();
+      }
+    });
 
+    // Log the brand name
+    cy.get('.brand').then((logoElement) => {
+      cy.log(logoElement.text());
+    });
+
+    cy.get('.search-keyword').clear().type('cucu');
+    cy.get('.product > .product-name').should('have.text', 'Cucumber - 1 Kg');
+    cy.get('.cart-icon > img').click();
   });
 });
